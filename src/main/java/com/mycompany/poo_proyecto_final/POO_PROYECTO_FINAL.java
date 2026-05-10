797afcf02976f5d666c8ca4a4f02f691a22ca101
samuelmurilloa <samuel.murillo_a@uao.edu.co>
2026-05-10 11:03:01 -0500

    Primer commit del proyecto, inicializar repo

---
+++ src/main/java/com/mycompany/poo_proyecto_final/POO_PROYECTO_FINAL.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poo_proyecto_final;
+
+/**
+ *
+ * @author samue
+ */
+public class POO_PROYECTO_FINAL {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
